5c8b4e672bf1c5965e2a331fb79593482ddf0e55
katieolson84 <katieolson84@gmail.com>
2021-04-19 19:01:30 -0600

    faq added

--- portfolio/src/Pages/AboutMe.js
+++ portfolio/src/Pages/AboutMe.js
@@ -2,12 +2,14 @@ import React from 'react'
 // import page components
 import About from '../components/About';
 import Work from '../components/Work';
+import Faq from '../components/Faq';
 
 const AboutMe = () => {
     return (
     <>
         <About />
         <Work />
+        <Faq />
     </>
     )
 }
