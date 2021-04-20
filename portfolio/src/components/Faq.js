1e1bb128756c8270f46e78f14b6bb3d48184c9c1
katieolson84 <katieolson84@gmail.com>
2021-04-20 17:00:37 -0600

    faq styling

--- portfolio/src/components/Faq.js
+++ portfolio/src/components/Faq.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import styled from 'styled-components';
+// styles
+import {Container} from '../styles';
 
 const Faq = () => {
     return (
-        <div>
+        <FaqContainer>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -11,6 +14,7 @@ const Faq = () => {
                     <p>Lorem ipsum sit amet. </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In eaque id maxime, quibusdam laudantium nam consectetur perferendis ea explicabo iure.</p>
                 </div>
+                <div className="faq-line"></div>
                 <div className="question">
                 <h4>Daily Schedule</h4>
             </div>
@@ -18,6 +22,7 @@ const Faq = () => {
                     <p>Lorem ipsum sit amet. </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In eaque id maxime, quibusdam laudantium nam consectetur perferendis ea explicabo iure.</p>
                 </div>
+                <div className="faq-line"></div>
                 <div className="question">
                 <h4>Different Payment Methods</h4>
             </div>
@@ -25,15 +30,43 @@ const Faq = () => {
                     <p>Lorem ipsum sit amet. </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In eaque id maxime, quibusdam laudantium nam consectetur perferendis ea explicabo iure.</p>
                 </div>
+                <div className="faq-line"></div>
                 <div className="question">
                 <h4>What products do you offer?</h4>
             </div>
                 <div className="answer">
                     <p>Lorem ipsum sit amet. </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In eaque id maxime, quibusdam laudantium nam consectetur perferendis ea explicabo iure.</p>
-                </div>
-        </div>
+                </div> 
+        </FaqContainer>
     )
 }
+
+const FaqContainer = styled(Container)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question{
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`
 
 export default Faq
--- portfolio/src/components/GlobalStyles.js
+++ portfolio/src/components/GlobalStyles.js
@@ -40,7 +40,7 @@ const GlobalStyle = createGlobalStyle`
 
     h4{
         font-weight: bold;
-       
+        font-size: 2rem;
     }
 
     a{
