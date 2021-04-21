ef45ba58a66ab47ff8f099885284b040e580f352
katieolson84 <katieolson84@gmail.com>
2021-04-21 10:12:54 -0600

    React router set up

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,15 +2,31 @@ import React from 'react';
 // import Pages
 import AboutMe from './Pages/AboutMe';
 import Nav from './components/Nav';
+import ContactMe from './Pages/ContactMe';
+import MyWork from './Pages/MyWork';
 // Global style
 import GlobalStyle from './components/GlobalStyles';
+// Router
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutMe />
+
+      <Switch>
+        <Route path="/work">
+          <MyWork />
+        </Route>
+        <Route path="/contact">
+          <ContactMe />
+        </Route>
+        <Route exact path="/">
+          <AboutMe />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
