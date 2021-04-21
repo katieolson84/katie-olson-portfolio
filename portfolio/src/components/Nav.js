73a6df96a45b51e4c4fa2250b4d1a660718b7635
katieolson84 <katieolson84@gmail.com>
2021-04-21 09:49:07 -0600

    nav styling

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -3,10 +3,48 @@ import styled from 'styled-components';
 
 const Nav = () => {
     return (
-        <div>
-            <h1><a href='#</h1>
-        </div>
+        <NavContainer>
+            <h1><a  id="logo" href='#'>Katie Olson</a></h1>
+            <ul>
+                <li>
+                    <a href="#">About Me</a>
+                </li>
+                <li>
+                    <a href="#">My Work</a>
+                </li>
+                <li>
+                    <a href="#">Contact Me</a>
+                </li>
+            </ul>
+        </NavContainer>
     )
 }
+
+const NavContainer = styled.nav`
+    min-height: 10vh;
+    display: flex;
+    margin: auto;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 10rem;
+    background: #282828;
+    a{
+        color: white;
+        text-decoration: none;
+    }
+    ul{
+        display: flex;
+        list-style: none;
+    }
+    #logo{
+        font-size: 1.5rem;
+        font-family: "Lobster", cursive;
+        font-weight: lighter;
+    }
+    li{
+        padding-left: 10rem;
+        position: relative;
+    }
+`
 
 export default Nav
