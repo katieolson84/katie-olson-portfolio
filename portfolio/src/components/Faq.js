3e201e8ec293016baa01dc068831c0eb40d53b42
katieolson84 <katieolson84@gmail.com>
2021-04-29 12:35:53 -0600

    edited text in faq section

--- portfolio/src/components/Faq.js
+++ portfolio/src/components/Faq.js
@@ -1,37 +1,41 @@
 import React from 'react'
-import styled from 'styled-components';
 // styles
+import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
+import { AnimateSharedLayout } from 'framer-motion';
+import { useScroll } from './useScroll';
+import { scrollReveal } from '../animation';
 
 const Faq = () => {
+    const [element, controls] = useScroll();
     return (
-        <FaqContainer>
-            <h2>Any <span>Questions</span></h2>
-            <div className="question">
-                <h4>Do you do freelance projects?</h4>
-            </div>
-                <div className="answer">
-                    <p>Yes. </p>
-                    <p>I have teamed up with an experienced UX/UI designer to provide a complete package of services. Together we map out the design and layout of your build and communicate with you every step of the way. Contact me for more details.</p>
-                </div>
-                <div className="faq-line"></div>
-                <div className="question">
-                <h4>How long does each build take?</h4>
-            </div>
-                <div className="answer">
-                    <p>That depends. </p>
-                    <p>Every build is different. Each build is mapped out to determine the needs of the project and timeframes vary. The number of features and the complexity of the app are taken into consideration when determining the time needed to develop an app. We can discuss options if your project is on a deadline.</p>
-                </div>
-                <div className="faq-line"></div>
-                <div className="question">
-                <h4>What payment methods do you except.</h4>
-            </div>
-                <div className="answer">
-                    <p>PayPal, Cash App, or Venmo</p>
-                    <p>Only eletronic payment methods are accepted at this time and a deposit is due before any work begins. Trades and equity can be discussed.</p>
-                </div>
-                <div className="faq-line"></div>
-                
+        <FaqContainer
+            variants={scrollReveal}
+            ref={element}
+            animate={controls}
+            initial="hidden"
+        >
+            <h2>
+                Available for<span>Freelance</span>
+            </h2>
+            <AnimateSharedLayout>
+                <Toggle title="Services Offered">
+                    <div className="answer">
+                        <p>I have teamed up with an experienced UX/UI designer to provide a complete package of services. Together we create logos, dynamite websites, complete branding packages, and custom software. We map out the design and layout of your build and communicate with you every step of the way. Contact me for more details.</p>
+                    </div>
+                </Toggle>
+                <Toggle title="Timelines for Builds">
+                    <div className="answer">
+                    <p>Every build is different. Each build is mapped out to determine the needs of the project and timeframes may vary. The number of features and the complexity of the app are taken into consideration when determining the time needed to develop an app. We can discuss options if your project is on a deadline.</p>
+                    </div>
+                </Toggle>
+                <Toggle title="Payments Methods">
+                    <div className="answer">
+                    <p>Only eletronic payment methods (PayPal, Cash App or Venmo) are accepted at this time and a deposit is due before any work begins. Trades and equity can be discussed.</p>
+                    </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </FaqContainer>
     )
 }
