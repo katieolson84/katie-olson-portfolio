c49cdf829cdf3af2a4182081c20a4531225210ea
Katie Olson <71294527+katieolson84@users.noreply.github.com>
2022-01-18 13:56:05 -0700

    Update Freelance.js

--- portfolio/src/components/Freelance.js
+++ portfolio/src/components/Freelance.js
@@ -23,17 +23,17 @@ const Freelance = () => {
             <AnimateSharedLayout>
                 <Toggle title="Services Offered">
                     <div className="answer">
-                        <p>I have teamed up with an experienced UX/UI designer to provide a complete package of services. Together we create logos, dynamite websites, complete branding packages, and custom software. We map out the design and layout of your build and communicate with you every step of the way. Contact me for more details.</p>
+                        <p>I have teamed up with an experienced UX/UI designer to provide a complete package of services. Together we create logos, dynamite websites, complete branding packages, product and packaging design, and marketing material. We map out the design and layout of your build and communicate with you every step of the way. I also offer technical consulting services for webinar and event setup, email campaigns, presentation design, social media ad design, and video editing. Contact me for more details.</p>
                     </div>
                 </Toggle>
                 <Toggle title="Timeline for Builds">
                     <div className="answer">
-                    <p>Every build is different. Each build is mapped out to determine the needs of the project and timeframes may vary. The number of features and the complexity of the app are taken into consideration when determining the time needed to develop an app. We can discuss options if your project is on a deadline.</p>
+                    <p>Every build and project is different. Each build is mapped out to determine the needs of the project and timeframes may vary. The number of features and the complexity of the app are taken into consideration when determining the time needed to develop an app. We can discuss options if your project is on a deadline.</p>
                     </div>
                 </Toggle>
                 <Toggle title="Payments Methods">
                     <div className="answer">
-                    <p>Only eletronic payment methods (PayPal, Cash App or Venmo) are accepted at this time and a deposit is due before any work begins. Trades and equity can be discussed.</p>
+                    <p>Only eletronic payment methods (Credit Card, Direct Deposit, PayPal, Cash App, or Venmo) are accepted at this time and a deposit is due before any work begins. Trades and equity can be discussed and contract work is available at an hourly rate.</p>
                     </div>
                 </Toggle>
             </AnimateSharedLayout>
